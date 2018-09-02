dd1bde69b222936f5aaa53bbddd8f4cb829b1431
nantikan <60030049@kmitl.ac.th>
2018-09-02 13:16:12 +0700

    first years

---
+++ Hilight/Hilight/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hilight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First years");
+            Console.WriteLine("ทำกิจกรรมเยอะมาก");
+            Console.ReadKey();
+        }
+    }
+}
